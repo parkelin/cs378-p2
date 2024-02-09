4298eef4de7d23503b9c06531e85e289c9f2e6a2
Elin Park <elinpark@utexas.edu>
2024-02-08 21:45:08 -0600

    suggested sytax for img

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -12,8 +12,8 @@ const MenuItem = ({ title, image, description, price }) => {
             <div class="row">
                 <div class="col-4">
                     {/* Image */}
-                    {/* <img src="/images/{imageName}.png"></img> */}
-                    <img class="image" src={"images/" + image}  />
+                    <img src="/images/{imageName}.png"></img>
+                    {/* <img class="image" src={"images/" + image}/> */}
                 </div>
 
                 <div class="col-8">
